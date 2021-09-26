fe9a1e4d962528c54b1b9026977bf5544022f982
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-09-25 17:21:34 -0700

    create NDToken.sol and update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,22 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account: ", deployer.address);
+
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
-
   await greeter.deployed();
-
   console.log("Greeter deployed to:", greeter.address);
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
+
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");
 }
 
 main()
